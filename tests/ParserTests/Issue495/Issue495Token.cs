f2ef0191f25872f6a9240d7ff47303a5b93440c6
bd k <b91456894@gmail.com>
2024-11-01 19:47:12 +0800

    Make issue reproducable

--- tests/ParserTests/Issue495/Issue495Token.cs
+++ tests/ParserTests/Issue495/Issue495Token.cs
@@ -4,6 +4,91 @@ namespace ParserTests.Issue495;
 
 public enum Issue495Token
 {
+    EOF = 0,
+
+    #region Sugar
+    [Comment("//", "/*", "*/")]
+    Comment,
+    [Sugar("++")]
+    Increment,
+    [Sugar("--")]
+    Decrement,
+    [Sugar("+")]
+    Plus,
+    [Sugar("-")]
+    Minus,
+    [Sugar("*")]
+    Multiply,
+    [Sugar("/")]
+    Divide,
+    [Sugar("%")]
+    Remainder,
+    [Sugar(">")]
+    GreaterThan,
+    [Sugar("<")]
+    LessThan,
+    [Sugar("==")]
+    Equal,
+    [Sugar("!=")]
+    NotEqual,
+    [Sugar(">=")]
+    GreaterThanOrEqual,
+    [Sugar("<=")]
+    LessThanOrEqual,
+    [Sugar("=")]
+    Assign,
+    [Sugar("?=")]
+    BooleanAssign,
+    [Sugar("%=")]
+    RemainderAssign,
+    [Sugar("+=")]
+    PlusAssign,
+    [Sugar("-=")]
+    MinusAssign,
+    [Sugar("*=")]
+    MultiplyAssign,
+    [Sugar("/=")]
+    DivideAssign,
+    [Sugar("><")]
+    CompareAssign,
+    [Sugar("??=")]
+    NullColesleAssign,
+    [Sugar("=>")]
+    Arrow,
+    [Sugar("{")]
+    BlockStart,
+    [Sugar("}")]
+    BlockEnd,
+    [Sugar("[")]
+    ListStart,
+    [Sugar("]")]
+    ListEnd,
+    [Sugar("(")]
+    ParenStart,
+    [Sugar(")")]
+    ParenEnd,
+    [Sugar(":")]
+    Colon,
+    [Sugar("::")]
+    Imply,
+    [Sugar(",")]
+    Comma,
+    [Sugar(";")]
+    End,
+    [Sugar("!")]
+    Not,
+    [Sugar("$")]
+    DollarSign,
+    [Sugar(".")]
+    Dot,
+    [Sugar("&")]
+    Deref,
+    [Sugar("||")]
+    Or,
+    [Sugar("&&")]
+    And,
+    [Sugar("\\")]
+    Escape,
     [Sugar("\"")]
     [Push("defaultString")]
     StartQuote,
@@ -11,13 +96,58 @@ public enum Issue495Token
     [Mode("defaultString")]
     [Pop]
     EndQuote,
+    #endregion
+
+    #region Keywords
+    [Keyword("true")]
+    True,
+    [Keyword("false")]
+    False,
+    [Keyword("run")]
+    RunKeyword,
+    [Keyword("import")]
+    ImportKeyword,
+    [Keyword("function")]
+    FunctionKeyword,
+    [Keyword("while")]
+    WhileKeyword,
+    [Keyword("if")]
+    IfKeyword,
+    [Keyword("else")]
+    ElseKeyword,
+    [Keyword("do")]
+    DoKeyword,
+    [Keyword("class")]
+    ClassKeyword,
+    [Keyword("new")]
+    NewKeyword,
+    [Keyword("null")]
+    NullKeyword,
+    [Keyword("command")]
+    CommandKeyword,
+    [Sugar("@s")]
+    SelectorSelf,
+    [Sugar("@p")]
+    SelectorNearest,
+    [Sugar("@a")]
+    SelectorAllPlayers,
+    [Sugar("@r")]
+    SelectorRandomPlayer,
+    [Sugar("@e")]
+    SelectorAllEntities,
+    #endregion
+
+    [Int]
+    Int,
+    [Double]
+    Double,
+    [AlphaNumDashId]
+    Identifier,
+
     [UpTo("\"")]
     [Mode("defaultString")]
     StringValue,
-    [Sugar(";")]
-    End,
-    [AlphaNumDashId]
-    Identifier,
-    [Sugar("=")]
-    Assign
+
+    Variable,
+    Namespace,
 }
